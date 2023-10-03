b6fbb6d93379f2e06f10a77bc5c2d12c2cc92df7
Joseph Patron <70641773+neosep29@users.noreply.github.com>
2023-10-03 13:00:18 +0800

    Update add_item.php

--- add_item.php
+++ add_item.php
@@ -15,17 +15,22 @@ if (!isset($_SESSION['user_id'])) {
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <h2>Add Item</h2>
-    <form method="post" action="add_item_process.php">
-        <label for="name">Name:</label>
-        <input type="text" name="name" required>
-        <label for="description">Description:</label>
-        <textarea name="description"></textarea>
-        <label for="quantity">Quantity:</label>
-        <input type="number" name="quantity" required>
-        <label for="price">Price:</label>
-        <input type="number" step="0.01" name="price" required>
-        <button type="submit">Add Item</button>
-    </form>
+<div class="add-item-container">
+        <h2>Add Item</h2>
+        <form class="add-item-form" method="post" action="add_item_process.php">
+            <label for="item_name">Item Name:</label>
+            <input type="text" name="name" required>
+            <label for="description">Description:</label>
+            <input type="text" name="description" required>
+            <label for="quantity">Quantity:</label>
+            <input type="number" name="quantity" required>
+            <label for="barcode">Barcode:</label>
+            <input type="text" name="barcode">
+            <button type="submit">Add</button>
+        </form>
+        <div class="back-button">
+            <a href="index.php">Back to Menu</a>
+        </div>
+    </div>
 </body>
 </html>
