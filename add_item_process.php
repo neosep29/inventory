81bfc7dd7861d995a8c33151b3831b44da11e390
Joseph Patron <70641773+neosep29@users.noreply.github.com>
2023-10-03 13:00:30 +0800

    Update add_item_process.php

--- add_item_process.php
+++ add_item_process.php
@@ -5,9 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $description = $_POST['description'];
     $quantity = $_POST['quantity'];
-    $price = $_POST['price'];
+    $barcode = $_POST['barcode'];
 
-    $sql = "INSERT INTO items (name, description, quantity, price) VALUES ('$name', '$description', $quantity, $price)";
+    $sql = "INSERT INTO items (name, description, quantity, barcode) VALUES ('$name', '$description', $quantity, $barcode)";
     if ($conn->query($sql) === TRUE) {
         header("Location: view_items.php");
     } else {
