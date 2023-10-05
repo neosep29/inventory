39a7f76004b3e02c29eb883078c37ac180ec3ca8
Joseph Patron <70641773+neosep29@users.noreply.github.com>
2023-10-05 13:00:22 +0800

    Update view_items.php

--- view_items.php
+++ view_items.php
@@ -1,50 +1,87 @@
+<?php 
+include('nav.php'); 
+include('db_config.php');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+}
+
+// Define the number of items to display per page
+$itemsPerPage = 15;
+
+// Get the current page number from the URL, default to 1 if not provided
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+// Calculate the offset for the SQL query
+$offset = ($page - 1) * $itemsPerPage;
+
+// Fetch items for the current page
+$sql = "SELECT * FROM items LIMIT $itemsPerPage OFFSET $offset";
+$result = $conn->query($sql);
+
+// Calculate the total number of items
+$totalItemsSql = "SELECT COUNT(*) as total FROM items";
+$totalItemsResult = $conn->query($totalItemsSql);
+$totalItems = $totalItemsResult->fetch_assoc()['total'];
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>View Items</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
 
     </style>
 </head>
-<body>
+<h1 class="inventory-header">Inventory System</h1>
+<body class="view-item-body">
+    <h2>View Items</h2>
     <div class="view-container">
-        <h2>View Items</h2>
+            <table class="view-items-table">
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <!-- Add more table headers as needed -->
+                </tr>
+                <?php
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["name"] . "</td>";
+                    echo "<td>" . $row["description"] . "</td>";
+                    echo "<td>" . $row["quantity"] . "</td>";
+                    // Add more table data cells as needed
+                    echo "</tr>";
+                }
+                ?>
+            </table>
     </div>
-    <table class="view-items-table">
-        <tr>
-            <th>Item Name</th>
-            <th>Description</th>
-            <th>Quantity</th>
-        </tr>
-        <?php
-        include('db_config.php');
 
-        $sql = "SELECT name, description, quantity FROM items"; // Updated query
-        $result = $conn->query($sql);
+<div class="pagination">
+    <?php
+    // Calculate the number of pages
+    $totalPages = ceil($totalItems / $itemsPerPage);
+    
+    // Previous Page button
+    if ($page > 1) {
+        echo "<a href='view_items.php?page=" . ($page - 1) . "' class='pagination-button'>Previous Page</a>";
+    }
 
-        if ($result !== false && $result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["name"] . "</td>";
-                echo "<td>" . $row["description"] . "</td>";
-                echo "<td>" . $row["quantity"] . "</td>";
-                echo "</tr>";
-            }
-        } else {
-            echo "<tr><td colspan='3'>No items found</td></tr>";
-        }
+    // Next Page button
+    if ($page < $totalPages) {
+        echo "<a href='view_items.php?page=" . ($page + 1) . "' class='pagination-button'>Next Page</a>";
+    }
+    ?>
+</div>
 
-        $conn->close();
-        ?>
-    </table>
     <div class="back-button">
             <a href="index.php">Back to Menu</a>
-        </div>
+    </div>
+    <div class="footer">
+        <p>&copy; Joseph Patron || <?php echo date("Y"); ?> Inventory System</p>
+    </div>
 </body>
 </html>
