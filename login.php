5fc7636dba0d3798c7268a0eb4558a2a0aebad57
Joseph Patron <70641773+neosep29@users.noreply.github.com>
2023-10-05 12:59:12 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,28 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 include('db_config.php');
 
 if (isset($_SESSION['user_id'])) {
-    header("Location: index.php");
+    $userRole = $_SESSION['user_role'];
+    if ($userRole === 'admin') {
+        // If the user is an admin, redirect to the main page with a button for barcode logs
+        header("Location: index.php");
+    } else {
+        // If the user is not an admin, redirect to the main page without the barcode logs button
+        header("Location: index.php");
+    }
 }
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     // Use prepared statements to prevent SQL injection
-    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    $sql = "SELECT id, username, password, role FROM users WHERE username = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $username);
     $stmt->execute();
@@ -20,13 +31,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
         $hashed_password = $row["password"];
-    
+        $userRole = $row["role"];
+
         // Debugging output
         echo "Entered Username: $username<br>";
         echo "Hashed Password from the Database: $hashed_password<br>";
-    
+
         if (password_verify($password, $hashed_password)) {
             $_SESSION['user_id'] = $row["id"];
+            $_SESSION['user_role'] = $userRole;
             header("Location: index.php");
         } else {
             $error = "Invalid password.";
@@ -41,19 +54,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $conn->close();
 ?>
 
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
+    <title>LoginUPLB </title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body class="b1">
-    <div class="container">
-        <h2>Login</h2>
+    <h1 class="inventory-header">Inventory System</h1>
+    <div class="login-container">
+        <img src="css/GS-logo.png" alt="Your Logo" class="login-logo">
+        <h2>UPLB Graduate School</h2>
         <form method="post" action="login.php">
             <label for="username">Username:</label>
             <input type="text" name="username" required>
@@ -68,6 +80,8 @@ $conn->close();
         ?>
         <p class="create-account-link">Don't have an account? <a href="registration.php">Create an account</a></p>
     </div>
-
+    <div class="footer">
+        <p>&copy; Joseph Patron || <?php echo date("Y"); ?> Inventory System</p>
+    </div>
 </body>
 </html>
